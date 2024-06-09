0117ce549c37e8051f28f2bf5dfb84cdb2723c05
sonubhosle <sbhosle1011@gmil.com>
2024-06-09 19:45:09 +0530

    evining

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,34 +1,71 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import '../../Style/Sidebar.css';
 import { TbSettings2, TbSquareLetterA } from 'react-icons/tb';
+import { GoDotFill } from "react-icons/go";
 
 const Sidebar = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [activeLinks, setActiveLinks] = useState({}); // Keep track of active links for each dropdown
   const [openDropdowns, setOpenDropdowns] = useState({});
+  const location = useLocation();
+
+  useEffect(() => {
+    // Initialize active links for each dropdown
+    const initialActiveLinks = {};
+    initialActiveLinks[0] = 0; // Set the first link of the first dropdown as active by default
+    setActiveLinks(initialActiveLinks);
+  }, []);
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
 
   const toggleDropdown = (index) => {
-    setOpenDropdowns((prevState) => ({
-      ...prevState,
-      [index]: !prevState[index],
-    }));
+    setOpenDropdowns((prevState) => {
+      const updatedDropdowns = { [index]: !prevState[index] };
+
+      // Close all other dropdowns
+      Object.keys(prevState).forEach((key) => {
+        if (key !== index.toString()) {
+          updatedDropdowns[key] = false;
+        }
+      });
+
+      return updatedDropdowns;
+    });
   };
+
+  const handleSetActiveLink = (dropdownIndex, linkIndex) => {
+    setActiveLinks((prevState) => {
+      const updatedActiveLinks = { ...prevState };
+
+      // If the clicked link is already active, deactivate it
+      if (updatedActiveLinks[dropdownIndex] === linkIndex) {
+        updatedActiveLinks[dropdownIndex] = null;
+      } else {
+        updatedActiveLinks[dropdownIndex] = linkIndex;
+      }
+
+      return updatedActiveLinks;
+    });
+  };
+
+
 
   // Data for dropdowns
   const dropdownData = [
     {
       label: 'Dashboard',
+      icon: `bx bxs-dashboard `,
       links: [
-        { name: 'Admin Dashboard', path: '/admin-dashboard-1' },
+        { name: 'Admin Dashboard', path: '/' },
         { name: 'Employee Dashboard', path: '/employee-dashboard-1' },
       ],
     },
     {
       label: 'Employee',
+      icon: `bx bx-user`,
       links: [
         { name: 'All Employees', path: '/all-employee' },
         { name: 'Holidays', path: '/holiday' },
@@ -45,8 +82,10 @@ const Sidebar = () => {
         { name: 'Overtime', path: '/overtime' },
       ],
     },
+
     {
       label: 'Sales',
+      icon: `bx bx-circle-three-quarter`,
       links: [
         { name: 'Admin Dashboard', path: '/admin-dashboard-1' },
         { name: 'Employee Dashboard', path: '/employee-dashboard-1' },
@@ -54,6 +93,7 @@ const Sidebar = () => {
     },
     {
       label: 'Accounting',
+      icon: `bx bx-briefcase`,
       links: [
         { name: 'Admin Dashboard', path: '/admin-dashboard-1' },
         { name: 'Employee Dashboard', path: '/employee-dashboard-1' },
@@ -61,9 +101,49 @@ const Sidebar = () => {
     },
     {
       label: 'Payrolls',
+      icon: `bx bx-wallet`,
       links: [
         { name: 'Admin Dashboard', path: '/admin-dashboard-1' },
         { name: 'Employee Dashboard', path: '/employee-dashboard-1' },
+      ],
+    },
+    {
+      label: 'Reports',
+      icon: `bx bxs-report`,
+      links: [
+        { name: 'Expense Report', path: 'expense_report' },
+        { name: 'Payments Report', path: 'payments_report' },
+        { name: 'Employee Report', path: '/amployee_report' },
+        { name: 'Payslip Report', path: '/payslip_report' },
+        { name: 'Attendance Report', path: '/attendance_report' },
+        { name: 'Leave Report', path: '/leave_report' },
+        { name: 'Daily Report', path: '/daily_report' },
+      ],
+    },
+    {
+      label: 'Profile',
+      icon: `bx bx-user-plus`,
+      links: [
+        { name: 'Employee Profile', path: '/employee-profile' },
+        { name: 'Client Profile', path: '/client-profile' },
+      ],
+    },
+    {
+      label: 'Authentication',
+      icon: `bx bx-key`,
+      links: [
+        { name: 'Login', path: '/login' },
+        { name: 'Register', path: '/signup' },
+        { name: 'Forgot Password', path: '/forgot_password' },
+      ],
+    },
+    {
+      label: 'Subscriptions',
+      icon: `bx bx-time`,
+      links: [
+        { name: 'Subscriptions (Admin)', path: '/subscriptions' },
+        { name: 'Subscriptions (Company)', path: '/subscription_compony' },
+        { name: 'Subscribed Componies', path: '/subscribed_componies' },
       ],
     },
     // Add more dropdowns as needed
@@ -80,24 +160,43 @@ const Sidebar = () => {
           <i onClick={toggleSidebar} className="bx bx-menu menu" id="btn"></i>
         </div>
         <ul className="nav-list">
+        <div className="heading"> <p></p> Main</div>
           {dropdownData.map((dropdown, index) => (
-           <>
             <div className="dashboard_dropdown" key={index}>
               <button onClick={() => toggleDropdown(index)} className="dropdown-toggle">
-                <p className='name'><TbSquareLetterA size={20} /> {dropdown.label}</p>
+                <p className='name'> <i className={dropdown.icon}></i> <div className="label">{dropdown.label}</div></p>
                 <span className={`arrow ${openDropdowns[index] ? 'open' : ''}`}><i className="bx bx-chevron-down"></i></span>
               </button>
               <div className={`dashboard-menu ${openDropdowns[index] ? 'open' : ''}`}>
                 {dropdown.links.map((link, linkIndex) => (
-                  <Link to={link.path} key={linkIndex}><p></p> {link.name}</Link>
+                  <Link
+                    to={link.path}
+                    key={linkIndex}
+                    className={`sidebar-link ${activeLinks[index] === linkIndex ? 'active-link' : ''}`}
+                    onClick={() => handleSetActiveLink(index, linkIndex)}
+                  >
+                    <GoDotFill /> {link.name}
+                  </Link>
                 ))}
               </div>
             </div>
-           </>
           ))}
+          <div className="heading"> <p></p> Pages</div>
+          <div className="pages">
+          <Link className='page_link'><i class='bx bx-user-plus'></i> Users</Link>
+            <Link className='page_link'><i class='bx bx-buildings'></i> Componies</Link>
+            <Link className='page_link'><i class='bx bx-user-pin' ></i> Contacts</Link>
+            <Link className='page_link'><i class='bx bx-x-circle'></i> Promotion</Link>
+            <Link className='page_link'><i class='bx bx-x-circle'></i> Resignation</Link>
+            <Link className='page_link'><i class='bx bx-x-circle'></i> Termination</Link>
+            
+
+          </div>
         </ul>
       </div>
     </>
+
+
   );
 };
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,6 +8,8 @@ const Home = () => {
     return (
         <section className='home-section'>
           <Header />
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur modi nobis enim voluptatum voluptate illum inventore consequatur. Maxime voluptas inventore, harum reiciendis amet repellat iure molestias enim placeat unde eius!
+          </p>
         </section>
         
     )
